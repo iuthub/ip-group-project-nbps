0823d96645d3583d03d5b43f47bdb388caa5f6dd
Kobil Umarbekov <chillman997@gmail.com>
2020-05-04 00:42:11 +0500

    [F][+] Basic routing for Index, Menu, Contacts page added

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './pages/landing/landing.component';
+import { ErrorComponent } from './pages/error/error.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    ErrorComponent,
+    MenuComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule,
