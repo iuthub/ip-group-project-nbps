ed0f8f1bea638ee78210d42519d9f876e1b0117b
Bositkhon <bositkhon.sh@gmail.com>
2020-05-10 23:22:24 +0500

    report and user seeder

--- backend/database/seeds/UserSeeder.php
+++ backend/database/seeds/UserSeeder.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\User;
+use Faker\Factory;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -13,5 +15,21 @@ class UserSeeder extends Seeder
     public function run()
     {
         factory(User::class, 5)->create();
+        $user = User::create([
+            'name' => 'Test User',
+            'email' => 'test@site.local',
+            'password' => Hash::make('password')
+        ]);
+        $faker = Factory::create();
+        $user->profile()->create([
+            'firstname' => $faker->firstName,
+            'lastname' => $faker->lastName,
+            'birthday' => $faker->date('Y-m-d'),
+            'phone' => $faker->phoneNumber,
+            'country' => $faker->country,
+            'city' => $faker->city,
+            'postcode' => $faker->postcode,
+            'address' => $faker->address
+        ]);
     }
 }
